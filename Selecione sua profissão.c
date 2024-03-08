ec3b8225db008d600982e34ec8cbf29d0c61d154
pedro arthur <160028208+arthurpedr@users.noreply.github.com>
2024-03-07 23:32:27 -0300

    Add files via upload

---
+++ Selecione sua profissão.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main(){
+	int op;
+
+//Seleção de
+ printf("Escolha uma opção abaixo.\n");
+ printf("Autonomo.\n");
+ printf("agricultor.\n");
+ printf("auxiliar.\n");
+ printf("arquiteto.\n");
+ printf("advogado.\n");
+ printf("Digite o número correspondente á sua escolha.\n");
+ scanf("%d", &op);
+
+switch(op){
+case 1:
+	 printf("Você escolheu Autonomo.\n");
+	 break;
+case 2:
+	printf("Você escolheu agricultor.\n");
+	break;
+case 3:
+    printf("Você escolheu auxiliar.\n");
+    break;
+case 4:
+	printf("Você escolheu arquiteto.\n");
+	break;
+case 5:
+	printf("Você escolheu advogado.\n");
+	break;
+default:
+	printf("opção inválida. por favor, escolha um número de 1 a 5.\n");
+	break;
+
+} while(op < 1 || op > 5);
+	
+	return 0;	
+	
+}
